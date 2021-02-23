4c13870299560f95c531eae9cc7510134224665e
Fabian Hoszowski <fabix43@gmail.com>
2021-02-23 01:34:59 +0100

    updated repositories by findById method.

--- src/main/java/com/medicalrecords/medicalrecords/repositories/DoctorRepository.java
+++ src/main/java/com/medicalrecords/medicalrecords/repositories/DoctorRepository.java
@@ -3,7 +3,11 @@ package com.medicalrecords.medicalrecords.repositories;
 import com.medicalrecords.medicalrecords.entities.Doctor;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 public interface DoctorRepository extends UserRepository<Doctor> {
+
+    Optional<Doctor> findById( Long id );
+
 }
--- src/main/java/com/medicalrecords/medicalrecords/repositories/PatientRepository.java
+++ src/main/java/com/medicalrecords/medicalrecords/repositories/PatientRepository.java
@@ -1,9 +1,14 @@
 package com.medicalrecords.medicalrecords.repositories;
 
+import com.medicalrecords.medicalrecords.entities.Doctor;
 import com.medicalrecords.medicalrecords.entities.Patient;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 public interface PatientRepository extends UserRepository<Patient> {
+
+    Optional<Patient> findById( Long id );
+
 }
